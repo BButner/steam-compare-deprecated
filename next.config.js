6d6ad61aa8ffef6c7bedac8d76e14caafdba243d
BButner <butner.beau@gmail.com>
2022-09-06 11:40:31 -0500

    Remove unnecessary dependency

--- next.config.js
+++ next.config.js
@@ -13,15 +13,6 @@ const nextConfig = {
   images: {
     domains: ["avatars.akamai.steamstatic.com"],
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    })
-
-    return config
-  },
 }
 
 module.exports = nextConfig
