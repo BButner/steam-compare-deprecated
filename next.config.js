acb61237e9ec2060eb510862aefa74610e9b5e2a
BButner <butner.beau@gmail.com>
2022-09-06 23:08:40 -0500

    Switch to look at production

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:3001/:path*",
+        destination: "https://api.steamcompare.games/:path*",
       }
     ]
   },
